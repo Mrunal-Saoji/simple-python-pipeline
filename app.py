18b165b83fae1a698f47f95d5cfc0125966ab245
Mrunal Saoji <59284027+Mrunal-Saoji@users.noreply.github.com>
2024-10-01 12:01:01 +0000

    reading table list

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 import sys
 from config import DB_DETAILS
+from utils import get_tables
 
 
 def main():
     """Program takes at least one argument"""
     env = sys.argv[1]
     db = DB_DETAILS[env]
-    print(db)
-
+    tables = get_tables('table_list.txt')
+    for  table in tables['table_name']:
+        print(table)
 
 if __name__ == '__main__':
     main()
